8cb175d981808fd79d7c4b7ca834f24e10fb5cda
zhihuaiwen <1570631036@qq.com>
2024-12-21 01:17:53 +0800

    11

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {createApp} from 'vue';
 import App from './App.vue';
-import router from './router/index.js';
+import router from './router';
 import {createVuetify} from 'vuetify';
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -5,7 +5,7 @@
  */
 
 export async function loadFonts () {
-  const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'@/plugins/webfontloader.js')
+  const webFontLoader = await import(/* webpackChunkName: "webfontloader" */'webfontloader')
 
   webFontLoader.load({
     google: {
