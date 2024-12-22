0f685ae8c52f208fcb237c5327db2aa5c8685182
zhihuaiwen <1570631036@qq.com>
2024-12-23 00:57:43 +0800

    11

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  base: '/amazon-rec/', // 确保这里是你的仓库名
   plugins: [
     vue(),
     vuetify({
