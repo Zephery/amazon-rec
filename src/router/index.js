663be7d5e7939f53707a1f6f6b02ffc364df7925
zhihuaiwen <1570631036@qq.com>
2024-12-29 16:39:43 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import ProductList from '../components/ProductList.vue';
 import HelloWorld from "../components/HelloWorld.vue";
 import ProductDetail from '../components/ProductDetail.vue';
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify';
+import {fileURLToPath, URL} from 'node:url'
 
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
 export default defineConfig({
     base: '/amazon-rec/', // 确保这里是你的仓库名
-    plugins: [
-        vue(),
-        vuetify({
-            autoImport: true,
-        }),
-    ],
-    build: {
-        outDir: "docs"
+    plugins: [vue(), VueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: true,
+        open: true
     }
-});
+})
