d8992fafd22fcd457e2121747bcda365b34a335f
zhihuaiwen <1570631036@qq.com>
2024-12-29 15:18:02 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  base: '/amazon-rec/', // 确保这里是你的仓库名
-  plugins: [
-    vue(),
-    vuetify({
-      autoImport: true,
-    }),
-  ],
+    base: '/amazon-rec/', // 确保这里是你的仓库名
+    build: {
+        outDir: 'docs'
+    },
+    plugins: [
+        vue(),
+        vuetify({
+            autoImport: true,
+        }),
+    ],
 });
