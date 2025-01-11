014588ec050afcd4f3c559649441130a05871624
zhihuaiwen <1570631036@qq.com>
2025-01-11 22:17:32 +0800

    update

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ docker stop --name amazon-rec
 
 docker build . -t amazon-rec
 
-docker run --name amazon-rec -d -p 80 -p 443  amazon-rec
+docker run --name amazon-rec -d -p 80:80 -p 443:443  amazon-rec
