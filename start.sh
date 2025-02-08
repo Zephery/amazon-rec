a96d6d9fff9bb6eb434562fa0b85b11f8499ef62
zhihuaiwen <1570631036@qq.com>
2025-02-08 20:33:05 +0800

    update

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ docker build . -t amazon-rec
 
 docker run --name amazon-rec -d -p 80:80 -p 443:443  amazon-rec
 
-
-docker stop $(docker ps -a | grep "rec-flask" | awk '{print $1 }')
-docker rm $(docker ps -a | grep "rec-flask" | awk '{print $1 }')
-
-cd rec-flask && docker build . -t rec-flask
-
-docker run --name rec-flask -d -p 5000:5000  rec-flask
+#
+#docker stop $(docker ps -a | grep "rec-flask" | awk '{print $1 }')
+#docker rm $(docker ps -a | grep "rec-flask" | awk '{print $1 }')
+#
+#cd rec-flask && docker build . -t rec-flask
+#
+#docker run --name rec-flask -d -p 5000:5000  rec-flask
