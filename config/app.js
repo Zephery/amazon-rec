d86536b65860f7cf9bb461d82c82ea6639a78af6
zhihuaiwen <1570631036@qq.com>
2025-02-08 21:53:48 +0800

    update

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 function getBaseURl() {
     if (process.env.NODE_ENV === 'prod') { // 这里改为 process.env.NODE_ENV
-        return 'http://43.163.107.45:5000';
+        return 'https://rec.wenzhihuai.com/api';
     } else {
         return 'http://localhost:5000';
     }
