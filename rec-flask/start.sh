74448d0c075d3de04a45be3fc7694acb84378a6e
zhihuaiwen <1570631036@qq.com>
2025-02-12 00:27:08 +0800

    update

--- rec-flask/start.sh
+++ rec-flask/start.sh
@@ -4,7 +4,6 @@
 (
     cd /home/admin/frp_0.61.1_linux_amd64 || exit
     nohup ./frpc -c frpc.toml > /dev/null 2>&1 &
-    echo $! > /home/admin/frp_0.61.1_linux_amd64/frpc.pid
 )
 
 # 设置amazon-rec的Flask服务
