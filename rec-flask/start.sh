97e334536c9ce682bdf6edf243d677f1497d47f0
zhihuaiwen <1570631036@qq.com>
2025-02-12 00:01:21 +0800

    update

--- rec-flask/start.sh
+++ rec-flask/start.sh
@@ -10,6 +10,7 @@
 # 设置amazon-rec的Flask服务
 (
     cd /home/admin/amazon-rec/rec-flask/
+    python3 -m venv .venv
     source .venv/bin/activate
     nohup flask run --host=0.0.0.0 > /dev/null 2>&1 &
     echo $! > /home/admin/amazon-rec/rec-flask/flask.pid
