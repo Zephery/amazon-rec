6401fe33f5c9c3d896b6d76012658c1851dba1c3
zhihuaiwen <zhihuaiwen@tencent.com>
2025-03-07 20:16:48 +0800

    1

--- config/app.js
+++ config/app.js
@@ -2,7 +2,7 @@ function getBaseURl() {
     if (process.env.NODE_ENV === 'prod') { // 这里改为 process.env.NODE_ENV
         return 'https://rec.wenzhihuai.com/api';
     } else {
-        return 'http://localhost:5000';
+        return 'http://127.0.0.1:5000';
     }
 }
 
---
+++ rec-flask/doc/proxy.sh
@@ -0,0 +1,2 @@
+git config http.proxy 'http://127.0.0.1:1087'
+git config https.proxy 'http://127.0.0.1:1087'
