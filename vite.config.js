af9901b7084dd59f710838a1827ef80ebd869133
zhihuaiwen <1570631036@qq.com>
2025-05-30 23:04:39 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/dist/**','**/rec-flask/**']
+    }
+  },
   plugins: [
     vue(),
     vuetify({
